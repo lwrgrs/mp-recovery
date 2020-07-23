fd9c30bda115fc4ee349633bcd7535f370aea1c5
lwrgrs <lwrogers@ncsu.edu>
2020-07-23 14:48:18 -0400

    removed plots and added to new file that contains only plot scripts

--- Root_Weight_Analysis_2018_2020.R
+++ Root_Weight_Analysis_2018_2020.R
@@ -22,20 +22,6 @@ root1_split <- split(root1, list(root1$fungtrt, root1$fumtrt))
 
 root2_split <- split(root2, levels(root2$fungtrt))
 
-### histograms
-
-library(ggplot2)
-
-p <- ggplot(root1, aes(x = recovery)) + geom_density()
-
-p + facet_grid(fungtrt ~ fumtrt, labeller = labeller(fungtrt = labels, fumtrt = labels)) +
-  labs(title = "Density Distribution of M. phaseolina Recovery for 2018 Field Trial", x = "Recovery Proportion", y = "Density")
-
-root1$fumtrt <- as.character(root1$fumtrt)
-root1$fungtrt <- as.character(root1$fungtrt)
-
-labels <- c("3" = "Chloropicrin", "6" = "No Fumigation Treatment", "4" = "2X Priaxor", "5" = "No Fungicide Treatment")
-
 #######
 ##########
 #############
@@ -109,15 +95,7 @@ summary(aov7)
 mean(root2_split[[3]]$weight)
 mean(root2_split[[3]]$recovery)
 
-### some box plots
-
-boxplot(root1_split[[4]]$weight, horizontal = T)
-
-
 ##########################################################################################################
-
-
-#### 7.14.2020 two sample t test
 
 path <- "/Users/lwrogers/Desktop/Macrophomina Project(s)/Recovery Data/Plated_Root_Weights_2018-20.xlsx"
 
@@ -192,115 +170,3 @@ t.test(recovery ~ weight_value, split_month[[9]])
 # 1.24.19
 
 t.test(recovery ~ weight_value, split_month[[10]])
-
-
-### layered plot attempt -- sample month on x axis, recovery on one y axis and root sample
-### weight on other y axis
-
-# treatments
-
-trt1$date <- as.factor(trt1$date)
-
-trt_means <- as.data.frame(aggregate(trt1[,5:6], list(trt1$date), FUN = mean))
-trt_means$recovery <- c(trt_means$recovery * 100)
-
-trt_means$Group.1 <- as.factor(trt_means$Group.1)
-
-# controls
-
-control$date <- as.factor(control$date)
-
-control_means <- as.data.frame(aggregate(control[,5:6], list(control$date), FUN = mean))
-control_means$recovery <- c(control_means$recovery * 100)
-
-control_means$Group.1 <- as.factor(control_means$Group.1)
-
-library(ggplot2)
-library(dplyr)
-library(patchwork)
-library(hrbrthemes)
-
-### individual plots
-
-t1 <- ggplot(trt_means, aes(x = Group.1, y = recovery, group = 1)) +
-  geom_line(color="#69b3a2", size=2) +
-  ggtitle("Mean M. phaseolina Recovery for Priaxor Treatment") +
-  theme_ipsum()
-
-t2 <- ggplot(trt_means, aes(x = Group.1, y = weight, group = 1)) +
-  geom_line(color="grey",size=2) +
-  ggtitle("Mean Root Sample Weight for each sampling date") +
-  theme_ipsum()
-
-t1 + t2
-
-### to layer the plots: treatment data
-
-ggplot(trt_means, aes(x = Group.1, group = 1)) +
-  geom_line(aes(y=recovery), size=2, color="#69b3a2") +
-  geom_line(aes(y=weight*100), size=2, color="blue") +
-  scale_y_continuous(name = "Mean Percent Recovery of M. phaseolina",
-                     sec.axis = sec_axis(trans=~./100, name="Mean Root Sample Weight")) +
-  theme_ipsum() +
-  xlab("Sampling Date") +
-  ggtitle("Mean Recovery and Sample Weight for each Sampling Date: 2X Priaxor at 2 Applications") +
-  theme(axis.title.y = element_text(size = 13, hjust = 0.5, vjust = 2, color = "#69b3a2"),
-        axis.title.y.right = element_text(size = 13, hjust = 0.5, vjust = 2, angle = 270, color = "blue"),
-        axis.title.x = element_text(size = 13, hjust = 0.5, vjust = 2),
-        plot.title = element_text(color = "black", hjust = 0.5))
-
-### write to file
-
-library(devEMF)
-
-setwd("/Users/lwrogers/Desktop/Macrophomina Project(s)/PH Breif/")
-
-emf(file = "Treatment_rec_wt.EMF", width = 10, height = 5)
-
-ggplot(trt_means, aes(x = Group.1, group = 1)) +
-  geom_line(aes(y=recovery), size=2, color="#69b3a2") +
-  geom_line(aes(y=weight*100), size=2, color="blue") +
-  scale_y_continuous(name = "Mean Recovery of M. phaseolina (%)",
-                     sec.axis = sec_axis(trans=~./100, name="Mean Root Sample Weight (g)")) +
-  theme_ipsum() +
-  xlab("Sampling Date") +
-  ggtitle("Mean Recovery and Sample Weight for each Sampling Date: 2X Priaxor at 2 Applications") +
-  theme(axis.title.y = element_text(size = 13, hjust = 0.5, vjust = 2, color = "#69b3a2"),
-        axis.title.y.right = element_text(size = 13, hjust = 0.5, vjust = 2, angle = 270, color = "blue"),
-        axis.title.x = element_text(size = 13, hjust = 0.5, vjust = 2),
-        plot.title = element_text(color = "black", hjust = 0.5))
-
-dev.off()
-
-
-### control data
-
-ggplot(control_means, aes(x = Group.1, group = 1)) +
-  geom_line(aes(y=recovery), size=2, color="#69b3a2") +
-  geom_line(aes(y=weight*100), size=2, color="blue") +
-  scale_y_continuous(name = "Mean Percent Recovery of M. phaseolina",
-                     sec.axis = sec_axis(trans=~./100, name="Mean Root Sample Weight")) +
-  theme_ipsum() +
-  xlab("Sampling Date") +
-  ggtitle("Mean Recovery and Sample Weight for each Sampling Date: Control") +
-  theme(axis.title.y = element_text(size = 13, hjust = 0.5, vjust = 2, color = "#69b3a2"),
-        axis.title.y.right = element_text(size = 13, hjust = 0.5, vjust = 2, angle = 270, color = "blue"),
-        axis.title.x = element_text(size = 13, hjust = 0.5, vjust = 2),
-        plot.title = element_text(color = "black", hjust = 0.5))
-
-emf(file = "control_rec_wt.emf", width = 10, height = 5)
-
-ggplot(control_means, aes(x = Group.1, group = 1)) +
-  geom_line(aes(y=recovery), size=2, color="#69b3a2") +
-  geom_line(aes(y=weight*100), size=2, color="blue") +
-  scale_y_continuous(name = "Mean Recovery of M. phaseolina (%)",
-                     sec.axis = sec_axis(trans=~./100, name="Mean Root Sample Weight (g)")) +
-  theme_ipsum() +
-  xlab("Sampling Date") +
-  ggtitle("Mean Recovery and Sample Weight for each Sampling Date: Control") +
-  theme(axis.title.y = element_text(size = 13, hjust = 0.5, vjust = 2, color = "#69b3a2"),
-        axis.title.y.right = element_text(size = 13, hjust = 0.5, vjust = 2, angle = 270, color = "blue"),
-        axis.title.x = element_text(size = 13, hjust = 0.5, vjust = 2),
-        plot.title = element_text(color = "black", hjust = 0.5))
-
-dev.off()
